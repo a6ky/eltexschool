4670422ba1302aa0f10e735190b54e0fccd2cbd7
Aleksey <stolpovsky6@gmail.com>
2021-03-30 15:40:06 +0700

    add args parser

--- process/my_bash.c
+++ process/my_bash.c
@@ -6,6 +6,41 @@
 #include <string.h>
 
 #define BUF_SIZE 256
+#define ARGS_BUF_SIZE 64
+#define ARGS_DELIM " \t\r\n\a"
+
+char **split_args(char *str)
+{
+    int bufsize = ARGS_BUF_SIZE;
+    int position = 0;
+    char **args = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!args) {
+        fprintf(stderr, "my_shell: ошибка выделения памяти");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(str, ARGS_DELIM);
+    while (token != NULL) {
+        args[position] = token;
+        position++;
+    
+        if (position >= bufsize) {
+            bufsize += ARGS_BUF_SIZE;
+            args = realloc(args, bufsize * sizeof(char*));
+            if (!args) {
+                fprintf(stderr, "my_shell: ошибка выделения памяти");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        token = strtok(NULL, ARGS_DELIM);
+    }
+
+    args[position] = NULL;
+    return args;
+}
 
 int exit_cmp(char *str)
 {
@@ -26,18 +61,18 @@ int main(int argc, char *argv[])
         name_bin[strlen(name_bin) - 1] = '\0'; 
 
         switch (child_pid = fork()) {
-            case -1:    /* ошибка */
+            case -1:
                 return -1;
 
-            case 0:     /* потомок */
-                ret = execlp(name_bin, "", (char *) NULL);
+            case 0:
+                ret = execvp(name_bin, split_args(name_bin));
             
                 if (ret == -1 && exit_cmp(name_bin) != 0) {
                     printf("unknown command\n");
                     exit(1);
                 }
                 
-            default:    /* родитель */
+            default:
                 if (waitpid(child_pid, &wstatus, 0) == -1)
                     return -1;
         }
